c0946af701cde73e5380b4524977743f888dbce3
Allan <luminussora@gmail.com>
2020-10-26 00:40:06 -0300

    review in question 1

--- question1.py
+++ question1.py
@@ -1,7 +1,16 @@
 def question1(arr, target):
-    for item in arr:
-        if -1 < (target - item) and arr.index(target - item):
-            return [arr.index(item), arr.index(target - item)] 
+    aux = arr.copy()
+    aux.sort()
+    start = 0
+    end = len(arr) - 1
+    while (start != end):
+        if (aux[start] + aux[end] > target):
+            end -= 1
+        elif (aux[start] + aux[end] < target):
+            start += 1
+        else:
+            return [arr.index(aux[start]), arr.index(aux[end])]
+
 
 if __name__ == '__main__':
     print("Array: %s Alvo: %d indices %s" % (str([2, 7, 11, 15]), 9, question1([2, 7, 11, 15], 9)))
